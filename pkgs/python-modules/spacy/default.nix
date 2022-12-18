3bba23bf3a01c43602f8959319a2bba66cd49278
Daniël de Kok <me@danieldk.eu>
2022-12-18 10:18:16 +0100

    python3Packages.spacy: distill-20221214 -> distill-20221215

--- pkgs/python-modules/spacy/default.nix
+++ pkgs/python-modules/spacy/default.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, spacy }:
 
 spacy.overrideAttrs (attrs: {
-  version = "distill-20221214";
+  version = "distill-20221215";
 
   src = fetchFromGitHub {
     owner = "danieldk";
     repo = "spacy";
-    rev = "55a1911b0777437b4eaef7d5f30f552794c66bf6";
-    hash = "sha256-eEpmggZhhc939zujF+6m61sZlnHxSDvOkNqhTRUkgPM=";
+    rev = "865f6a156f7790e7d38ed72583cfbee19bc7dcf0";
+    hash = "sha256-zXoxd5dAnvETyn9fmhEQ+6/pc4f4xSiTC/qXpIEuiOA=";
   };
 })
