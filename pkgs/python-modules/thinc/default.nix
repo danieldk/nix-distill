45c0a9bb031983537778b01df2b70f34a54b33f2
Daniël de Kok <me@danieldk.eu>
2022-12-23 15:39:55 +0100

    python3Packages.thinc: with-flatten-v2 -> 9.0.0-dev1

--- pkgs/python-modules/thinc/default.nix
+++ pkgs/python-modules/thinc/default.nix
@@ -1,12 +1,13 @@
 { fetchFromGitHub, thinc }:
 
-(thinc.overrideAttrs (attrs: {
-  version = "with-flatten-v2";
+(thinc.overrideAttrs (attrs: rec {
+  name = "${attrs.pname}-${version}";
+  version = "9.0.0.dev1";
 
   src = fetchFromGitHub {
-    owner = "danieldk";
+    owner = "explosion";
     repo = "thinc";
-    rev = "37db76f1bca6a59b329d3b67275dc735de13ed44";
-    hash = "sha256-FA0S5bqbHnS+pdPrrN4xfi0djofCAzNQ/1s8DgAq5IA=";
+    rev = "v9.0.0.dev1";
+    hash = "sha256-xKM0sKkMTamx/mNzzS0YTbcBn7Cn+lhheB3/cK8GFgs=";
   };
 }))
